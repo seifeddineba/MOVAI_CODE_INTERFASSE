f922daed73c8bd7070cc90358c443adb9a73883f
seifeddineba <seifeddinebenaicha3@gmail.com>
2022-12-02 06:23:39 +0100

    first push

--- src/components/Modules/header/index.jsx
+++ src/components/Modules/header/index.jsx
@@ -8,7 +8,7 @@ function header() {
         <div className='header-container'>
             <div className='header-box'>
                 <img src={logo} alt="logo" width={150} height={150} />
-                <h1 className='sida-title'>シダ</h1>
+                <h1 className='sida-title'>SIDA</h1>
                 <div className='flex-end'>
                   <button className='btn-search'>recherche<i class="fa fa-trash" aria-hidden="true"></i></button>
                   <input type="password" placeholder='recherche...' className='input-search'/>
